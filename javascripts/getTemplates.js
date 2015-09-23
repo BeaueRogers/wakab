30310b872177301cdb0297560091f2f9825ad7ae
walkerdorris <walker.dorris@gmail.com>
2015-09-22 21:40:39 -0500

    working with bootstrap/handlebars

--- javascripts/getTemplates.js
+++ javascripts/getTemplates.js
@@ -1,15 +1,63 @@
-define(["hbs", "hbs!../templates/dogs", "hbs!../templates/cats"], function(Handlebars, dogTemplate, catTemplate) {
+define(["hbs", 
+  "hbs!../templates/dogNames", 
+  "hbs!../templates/dogTypes", 
+  "hbs!../templates/dogVolumes",
+  "hbs!../templates/catNames",
+  "hbs!../templates/catBreeds",
+  "hbs!../templates/catTypes",
+  "hbs!../templates/catVolumes"
+  ], function(Handlebars, dogNameTmpl, dogTypeTmpl, dogVolTmpl, catNameTmpl, catBreedTmpl, catTypeTmpl, catVolTmpl) {
 
-	//console.log("templates", Handlebars);
+  //console.log("templates", Handlebars);
 
   var templates = {};
   // Create empty object to contain my hbs templates
-  templates.dogShit = dogTemplate;
-  templates.catShit = catTemplate;
+  templates.dogName = dogNameTmpl;
+ // console.log('dogNameTmpl', dogNameTmpl);
 
+  templates.dogType = dogTypeTmpl;
+ //console.log('dogTypeTmpl', dogTypeTmpl);
+
+  templates.dogVol = dogVolTmpl;
+ // console.log('dogVolTmpl', dogVolTmpl);
+  
+  templates.catName = catNameTmpl;
+ // console.log('catNameTmpl', catNameTmpl);
+
+  templates.catBreed = catBreedTmpl;
+ // console.log('catNameTmpl', catNameTmpl);
+
+  templates.catType = catTypeTmpl;
+ // console.log('catTypeTmpl', catTypeTmpl);
+
+  templates.catVol = catVolTmpl;
+ // console.log('catVolTmpl', catVolTmpl);
 
   return templates;
+
 });
+
+
+
+
+
+
+
+
+
+// define(["hbs", "hbs!../templates/dogs", "hbs!../templates/cats", "hbs!../templates/dogbrand"], function(Handlebars, dogTemplate, catTemplate, dogBrandTemplate) {
+
+//   //console.log("templates", Handlebars);
+
+//   var templates = {};
+//   // Create empty object to contain my hbs templates
+//   templates.dogShit = dogTemplate;
+//   templates.catShit = catTemplate;
+//   templates.dogBrand = dogBrandTemplate;
+
+
+//   return templates;
+// });
 
   /* Assign the key of songs to my object and set it equal to the value of my songs template
   templates.artists = artistTemplate;
@@ -19,3 +67,33 @@ define(["hbs", "hbs!../templates/dogs", "hbs!../templates/cats"], function(Handl
   return templates;
 });
 */
+
+
+
+
+
+
+
+
+
+
+// define(["hbs", "hbs!../templates/dogs", "hbs!../templates/cats"], function(Handlebars, dogTemplate, catTemplate) {
+
+// 	//console.log("templates", Handlebars);
+
+//   var templates = {};
+//   // Create empty object to contain my hbs templates
+//   templates.dogShit = dogTemplate;
+//   templates.catShit = catTemplate;
+
+
+//   return templates;
+// });
+
+//    Assign the key of songs to my object and set it equal to the value of my songs template
+//   templates.artists = artistTemplate;
+//   // Assign the key of songs to my object and set it equal to the value of my artist template
+//   templates.albums = albumTemplate;
+//   // Assign the key of songs to my object and set it equal to the value of my album template
+//   return templates;
+// });
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,3 @@
-
 requirejs.config({
   baseUrl: './javascripts',
   paths: {
@@ -18,27 +17,26 @@ requirejs.config({
 });
 
 require(
-  ["jquery", "lodash", "firebase", "bootstrap", "getUnique",
-   "getTemplates"], 
-   function($, _, firebase, bootstrap, unique, templates) {
-    var dogBrandsObject = {};
-  
+  ["jquery", "firebase", "bootstrap", "getTemplates"], 
+   function($, firebase, bootstrap, templates) {
+    
     // Create a reference to your Firebase database
     var myFirebaseRef = new Firebase("https://acme-product-lines.firebaseio.com");
 
     // Listen for when anything changes on the "dog_brands" key
     myFirebaseRef.child("dog_brands").on("value", function(snapshot) {
 
-    	console.log('snapshot.val()', snapshot.val());
+      console.log('snapshot.val()', snapshot.val());
 
-    	// Store the entire dogFood key in a local variable
-    	var dogData = snapshot.val();
-    
+      // Store the value of the dog_brands key in a local variable
+      var dogData = snapshot.val();
 
       var dogObject = {dogBrands: dogData};
       console.log('dogObject', dogObject);
 
-      $("#dog-brands").html(templates.dogShit(dogObject));     
+      $("#dogNames").html(templates.dogName(dogObject));
+      $("#dogTypes").html(templates.dogType(dogObject));
+      $("#dogVolumes").html(templates.dogVol(dogObject));         
 
 
   });
@@ -47,16 +45,161 @@ require(
 
       console.log('snapshot.val()', snapshot.val());
 
-      // Store the entire dogFood key in a local variable
+      // Store the value of the cat_brands key in a local variable
       var catData = snapshot.val();
     
 
       var catObject = {catBrands: catData};
       console.log('catObject', catObject);
 
-      $("#cat-brands").html(templates.catShit(catObject));      
+      $("#catNames").html(templates.catName(catObject));
+      $("#catBreeds").html(templates.catBreed(catObject));
+      $("#catTypes").html(templates.catType(catObject));
+      $("#catVolumes").html(templates.catVol(catObject));      
 
 
-  });
+  }); 
 
 });
+
+
+
+
+
+
+
+
+
+
+// requirejs.config({
+//   baseUrl: './javascripts',
+//   paths: {
+//     'jquery': '../lib/bower_components/jquery/dist/jquery.min',
+//     'firebase': '../lib/bower_components/firebase/firebase',
+//     'lodash': '../lib/bower_components/lodash/lodash.min',
+//     'hbs': '../lib/bower_components/require-handlebars-plugin/hbs',
+//     'bootstrap': '../lib/bower_components/bootstrap/dist/js/bootstrap.min'
+//   },
+
+//   shim: {
+//     'bootstrap': ['jquery'],
+//     'firebase': {
+//       exports: 'Firebase'
+//     }
+//   }
+// });
+
+// require(
+//   ["jquery", "lodash", "firebase", "bootstrap", "getUnique",
+//    "getTemplates"], 
+//    function($, _, firebase, bootstrap, unique, templates) {
+//     var dogBrandsObject = {};
+  
+//     // Create a reference to your Firebase database
+//     var myFirebaseRef = new Firebase("https://acme-product-lines.firebaseio.com");
+
+//     // Listen for when anything changes on the "dog_brands" key
+//     myFirebaseRef.child("dog_brands").on("value", function(snapshot) {
+
+//     	console.log('snapshot.val()', snapshot.val());
+
+//     	// Store the entire dogFood key in a local variable
+//     	var dogData = snapshot.val();
+    
+
+//       var dogObject = {dogBrands: dogData};
+//       console.log('dogObject', dogObject);
+
+//       $("#dog-brands").html(templates.dogShit(dogObject));     
+
+
+//   });
+
+//     myFirebaseRef.child("cat_brands").on("value", function(snapshot) {
+
+//       console.log('snapshot.val()', snapshot.val());
+
+//       // Store the entire dogFood key in a local variable
+//       var catData = snapshot.val();
+    
+
+//       var catObject = {catBrands: catData};
+//       console.log('catObject', catObject);
+
+//       $("#cat-brands").html(templates.catShit(catObject));      
+
+
+//   });
+
+// });
+
+
+
+
+
+
+
+
+
+
+// requirejs.config({
+//   baseUrl: './javascripts',
+//   paths: {
+//     'jquery': '../lib/bower_components/jquery/dist/jquery.min',
+//     'firebase': '../lib/bower_components/firebase/firebase',
+//     'lodash': '../lib/bower_components/lodash/lodash.min',
+//     'hbs': '../lib/bower_components/require-handlebars-plugin/hbs',
+//     'bootstrap': '../lib/bower_components/bootstrap/dist/js/bootstrap.min'
+//   },
+
+//   shim: {
+//     'bootstrap': ['jquery'],
+//     'firebase': {
+//       exports: 'Firebase'
+//     }
+//   }
+// });
+
+// require(
+//   ["jquery", "lodash", "firebase", "bootstrap", "getUnique",
+//    "getTemplates"], 
+//    function($, _, firebase, bootstrap, unique, templates) {
+//     var dogBrandsObject = {};
+  
+//     // Create a reference to your Firebase database
+//     var myFirebaseRef = new Firebase("https://acme-product-lines.firebaseio.com");
+
+//     // Listen for when anything changes on the "dog_brands" key
+//     myFirebaseRef.child("dog_brands").on("value", function(snapshot) {
+
+//     	console.log('snapshot.val()', snapshot.val());
+
+//     	// Store the entire dogFood key in a local variable
+//     	var dogData = snapshot.val();
+    
+
+//       var dogObject = {dogBrands: dogData};
+//       console.log('dogObject', dogObject);
+
+//       $("#dog-brands").html(templates.dogShit(dogObject));     
+
+
+//   });
+
+//     myFirebaseRef.child("cat_brands").on("value", function(snapshot) {
+
+//       console.log('snapshot.val()', snapshot.val());
+
+//       // Store the entire dogFood key in a local variable
+//       var catData = snapshot.val();
+    
+
+//       var catObject = {catBrands: catData};
+//       console.log('catObject', catObject);
+
+//       $("#cat-brands").html(templates.catShit(catObject));      
+
+
+//   });
+
+// });
